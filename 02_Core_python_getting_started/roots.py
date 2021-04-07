e6c866d96d0f637b1bfd906b1ace7d822f60652b
cannatx <nachocane@msn.com>
2021-04-07 05:37:16 +0000

    0208_06_ValueError

--- 02_Core_python_getting_started/roots.py
+++ 02_Core_python_getting_started/roots.py
@@ -1,3 +1,4 @@
+import sys
 
 def sqrt(x):
     """Compute sqare roott using the method
@@ -9,19 +10,33 @@ def sqrt(x):
 
     Returns:
         The sqare root of x.
+
+    Raise:
+        ValueError: If x is negative.
     """
+    if x < 0:
+        raise ValueError(
+            "Cannot compute sqare root of "
+            f"negative number {x}"
+        )
+
     guess = x
     i = 0
     while guess * guess != x and i < 20:
         guess = (guess + x / guess) / 2.0
         i += 1
+
     return guess
 
 
 def main():
-    print(sqrt(9))
-    print(sqrt(2))
-    print(sqrt(-1))
+    try:
+        print(sqrt(9))
+        print(sqrt(2))
+        print(sqrt(-1))
+        print("This is never printed")
+    except ValueError as e:
+        print(e, file=sys.stderr)
 
 
 if __name__ == '__main__':
